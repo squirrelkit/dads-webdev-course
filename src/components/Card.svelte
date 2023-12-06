6ba4e833e24560241bea73a3718f1de9a5f2cc49
squirrelkit <kayapeiman@gmail.com>
2023-12-06 10:17:15 -0400

    nice, simple card aligned  just as I want it

---
+++ src/components/Card.svelte
@@ -0,0 +1,51 @@
+<div class="flex card-wrapper">
+    <div class="image-wrapper">
+        <img class="image" src="https://picsum.photos/200" alt="image" />
+    </div>
+
+    <div class="card-content-wrapper">
+        <h1 class="text">Hello</h1>
+        <p class="text">This is a paragraph</p>
+    </div>
+</div>
+
+<style>
+    .image-wrapper {
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+    }
+    .flex {
+        display: flex;
+    }
+
+    .card-wrapper {
+        background: linear-gradient(to bottom right, #abcdef, #123456);
+    }
+
+    .card-content-wrapper {
+
+    }
+
+    .card {
+        border: 1px solid #123456;
+        background: linear-gradient(to bottom right, #abcdef, #123456);
+        padding: 10px;
+        margin: 10px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .image {
+        width: 200px;
+        height: 200px;
+    }
+
+    .text {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
