47dd0f473968fdefdcc71a5c6e21b2177cf83911
squirrelkit <kayapeiman@gmail.com>
2023-12-09 14:36:17 -0400

    updated my exercise post for today

--- src/routes/exercises/2023-12-09/+page.svelte
+++ src/routes/exercises/2023-12-09/+page.svelte
@@ -1,3 +1,8 @@
 <h2 class="flex justify-center">2023-12-09</h2>
 
-<p class="flex justify-center">I have gotten started with skeleton and tailwind and I am very, very excited.</p>
+<p class="flex justify-center">I have gotten started with skeleton and tailwind and I am very, very excited.
+    
+    Just tried making a commit from the source control right here in vscode. If you go to vscode you'll see in the left sidebar, the third icon. You can commit from there. (I think. Not certain if it worked.)
+    
+    Ok on checking.... It didn't work. idk why it didn't work.
+</p>
