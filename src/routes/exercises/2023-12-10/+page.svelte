625f61797f4a66590ea9c16cd142dd83e8075f35
squirrelkit <kayapeiman@gmail.com>
2023-12-10 16:06:24 -0400

    trying to use canvas, not seeing anything

---
+++ src/routes/exercises/2023-12-10/+page.svelte
@@ -0,0 +1,13 @@
+
+<script>
+    import CanvasQuick from "./CanvasQuick.svelte";
+</script>
+
+<style>
+    <canvas bind:this{canvas}/>
+</style>
+<h2 class="flex justify-center">2023-12-10</h2>
+
+<p class="flex justify-center">
+</p>
+
---
+++ src/routes/exercises/2023-12-10/CanvasQuick.svelte
@@ -0,0 +1,49 @@
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		console.log('canvas component mounted');
+		('canvas component mounted');
+		sketchOnCanvas();
+	});
+
+	let width = 0;
+	let height = 0;
+
+	$: if (typeof window !== 'undefined') {
+		width = window.innerWidth;
+		console.log("🚀 ~ file: CanvasQuick.svelte:15 ~ width:", width)
+		height = window.innerHeight;
+		console.log("🚀 ~ file: CanvasQuick.svelte:17 ~ height:", height)
+	}
+
+	function sketchOnCanvas() {
+		const c = document.querySelector('canvas');
+		const pen = canvas.getContext('2d');
+
+		pen.beginPath();
+		pen.arc(center.x, center.y, 0, 2, Math.PI * 2);
+		pen.linewidth = 2;
+		pen.strokeStyle = 'pink';
+		pen.stroke();
+	}
+
+	function dataChanged(data) {
+		if (manager) {
+			Object.assign(manager.props.data, data);
+			manager.render();
+		}
+	}
+	
+</script>
+
+<canvas bind:this={canvas} />
+
+<style>
+	/* Optionally style the canvas here */
+	canvas {
+		margin: auto;
+		display: block;
+		box-shadow: 0px 2px 12px -2px rgba(0, 0, 0, 0.15);
+	}
+</style>
