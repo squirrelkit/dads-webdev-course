d4e0effab2d6b2c5d9e3b9c3f89592141ce385ce
squirrelkit <kayapeiman@gmail.com>
2023-12-11 12:40:53 -0400

    barely done anything, but my update page for today is no longer correct, and orbit is STILL not working. as in i'm not seeing anythins

--- src/routes/exercises/2023-12-11/+page.svelte
+++ src/routes/exercises/2023-12-11/+page.svelte
@@ -1,4 +1,4 @@
-<div>
-    <h2 class="flex justify-center">2023-12-11 Monday</h2>
-    <p class="flex justify-center">Yesterday, Dad managed to get an object orbiting around a fixed origin. Today I am going to try to do the same.</p>
+<div class="w-2/5 flex justify-center items-center flex-col">
+    <h2>2023-12-11 Monday</h2>
+    <p>Yesterday, Dad managed to get an object orbiting around a fixed origin. Today I am going to try to do the same.</p>
 </div>
