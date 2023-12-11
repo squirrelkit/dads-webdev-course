7dd611aa244b6856fe7445312fcb34597ff6d8d7
squirrelkit <kayapeiman@gmail.com>
2023-12-11 12:23:06 -0400

    formatted a paragrph width

--- src/routes/exercises/2023-12-09/+page.svelte
+++ src/routes/exercises/2023-12-09/+page.svelte
@@ -1,9 +1,7 @@
-<h2 class="flex justify-center">2023-12-09</h2>
-
-<p class="flex justify-center">I have gotten started with skeleton and tailwind and I am very, very excited.
-
-    Just tried making a commit from the source control right here in vscode. If you go to vscode you'll see in the left sidebar, the third icon. You can commit from there. (I think. Not certain if it worked.)
-    
-    Ok on checking.... It didn't commit. idk why it didn't commit.
-</p>
-
+<div class="w-2/5 flex justify-center items-center flex-col">
+    <h2>2023-12-09 Saturday</h2>
+    <p>I have gotten started with skeleton and tailwind and I am very, very excited. Just tried making
+		a commit from the source control right here in vscode. If you go to vscode you'll see in the
+		left sidebar, the third icon. You can commit from there. (I think. Not certain if it worked.) Ok
+		on checking.... It didn't commit. idk why it didn't commit.</p>
+</div>
---
+++ src/routes/exercises/2023-12-11/+page.svelte
@@ -0,0 +1,4 @@
+<div>
+    <h2 class="flex justify-center">2023-12-11 Monday</h2>
+    <p class="flex justify-center">Yesterday, Dad managed to get an object orbiting around a fixed origin. Today I am going to try to do the same.</p>
+</div>
