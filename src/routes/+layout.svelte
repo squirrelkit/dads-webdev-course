176ec5282d5d6cb49d7b92ab516880f05dd973c5
squirrelkit <kayapeiman@gmail.com>
2023-12-12 11:31:59 -0400

    SKELETON home button is now a home icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,7 +14,9 @@
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong><button class="text-xl uppercase"><a href="/">Skeleton</a></button></strong>
+				<strong><button class="text-xl uppercase"><a href="/">
+					<svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 24 24"><path fill="currentColor" d="M10 20v-6h4v6h5v-8h3L12 3L2 12h3v8z"/></svg>
+				</a></button></strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
 				<a
