2006406f795d3b590d8a5710f268bb0b83730f32
squirrelkit <kayapeiman@gmail.com>
2023-12-14 12:02:51 -0400

    got part of a grid using codeium AI, not finished

--- src/routes/exercises/2023-12-14/+page.svelte
+++ src/routes/exercises/2023-12-14/+page.svelte
@@ -1,7 +1,9 @@
+
+
 <div class="w-2/5 flex justify-center items-center flex-col">
 	<h2>2023-12-12 Tuesday</h2>
 	<p>
-		Today I have done genuinely nothing. But, tomorrow I will do something.
+		<a class="text-blue-500" href="http://localhost:5173/exercises/2023-12-14/1">Here</a>
 	
 	</p>
 </div>
---
+++ src/routes/exercises/2023-12-14/1/+page.svelte
@@ -0,0 +1,34 @@
+<!-- HexGrid.svelte -->
+<style>
+    .grid {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
+    }
+  
+    .hexagon {
+      width: 100px;
+      height: 100px;
+      background-color: #ff0000;
+      clip-path: polygon(50% 0%, 100% 25%, 100% 75%, 50% 100%, 0% 75%, 0% 25%);
+      margin: 5px;
+    }
+  </style>
+  
+  <script>
+    let hexGrid = [
+      [1, 1, 1, 1, 1],
+      [1, 1, 1, 1],
+      [1, 1, 1, 1, 1],
+      [1, 1, 1, 1],
+      [1, 1, 1, 1, 1]
+    ];
+  </script>
+  
+  <div class="grid">
+    {#each hexGrid as row}
+      {#each row as cell}
+        <div class="hexagon"></div>
+      {/each}
+    {/each}
+  </div>
