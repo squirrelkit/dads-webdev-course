c47d7274d1a911c72d216d3002e4c2a3ee458777
squirrelkit <kayapeiman@gmail.com>
2023-12-16 12:44:58 -0400

    canvas overflowing, text pixelated, way to big

--- src/components/Canvas.svelte
+++ src/components/Canvas.svelte
@@ -1,5 +1,48 @@
 <script>
-
-</script>
-
-<canvas id="canvas" width="300" height="200" style="border: 2px solid white"></canvas>
+    import { onMount } from 'svelte';
+  
+    let context;
+  
+    onMount(() => {
+      const canvas = document.getElementById('test-canvas');
+      context = canvas.getContext('2d');
+  
+      canvas.addEventListener('mousemove', function (e) {
+        var cRect = canvas.getBoundingClientRect();
+        var canvasX = Math.round(e.clientX - cRect.left);
+        var canvasY = Math.round(e.clientY - cRect.top);
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.font = '12px Arial'; // Set the font size here
+        context.fillText('X: ' + canvasX + ', Y: ' + canvasY, 10, 20);
+      });
+  
+      return () => {
+        // Cleanup event listener when component is destroyed
+        canvas.removeEventListener('mousemove', handleMouseMove);
+      };
+    });
+  </script>
+  
+  <style>
+    #test-canvas-container {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: 100vh; /* Full height of the viewport */
+    }
+  
+    #test-canvas {
+      width: 100%; /* Full width of the container */
+      height: 100%; /* Full height of the container */
+      border: 2px solid white;
+      image-rendering: optimizeSpeed; /* Older versions of Firefox */
+      image-rendering: -moz-crisp-edges; /* Firefox */
+      image-rendering: -webkit-optimize-contrast; /* Webkit (Safari/Chrome) */
+      image-rendering: optimize-contrast; /* Modern browsers */
+    }
+  </style>
+  
+  <div id="test-canvas-container">
+    <canvas id="test-canvas"></canvas>
+  </div>
+  
