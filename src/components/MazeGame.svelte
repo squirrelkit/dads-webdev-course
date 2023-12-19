60e985789e2f2bd827fafd7892b22870e6cb8322
squirrelkit <kayapeiman@gmail.com>
2023-12-19 12:24:25 -0400

    using gpt to make me a game

--- src/components/MazeGame.svelte
+++ src/components/MazeGame.svelte
@@ -1,149 +1,65 @@
 <script>
-    import { onMount } from 'svelte';
+    let player = { x: 50, y: 50, isJumping: false };
   
-    let mazeData = [
-      '####################',
-      '#S#                #',
-      '# #                #',
-      '# #                #',
-      '# #                #',
-      '# #                #',
-      '# #                #',
-      '# #                #',
-      '# #                #',
-      '# #                #',
-      '# #                #',
-      '# #                #',
-      '# #                #',
-      '# #                #',
-      '# #                #',
-      '# #                #',
-      '# #                #',
-      '#                  #',
-      '###################E'
-    ];
+    function handleKeyPress(event) {
+      const step = 50; // Adjusted to move 50 pixels
   
-    let playerTop = 20;
-    let playerLeft = 20;
-  
-    function movePlayer(direction) {
-      console.log("🚀 ~ file: MazeGame.svelte:30 ~ movePlayer ~ direction:", direction)
-      switch (direction) {
-        case 'up':
-          playerTop -= 10;
+      switch (event.key) {
+        case 'ArrowUp':
+          player.y = Math.max(0, player.y - step);
           break;
-        case 'down':
-          playerTop += 10;
+        case 'ArrowDown':
+          player.y = Math.min(80 * window.innerHeight / 100 - 20, player.y + step); // 80% of the viewport height
           break;
-        case 'left':
-          playerLeft -= 10;
+        case 'ArrowLeft':
+          player.x = Math.max(0, player.x - step);
           break;
-        case 'right':
-          playerLeft += 10;
+        case 'ArrowRight':
+          player.x = Math.min(80 * window.innerWidth / 100 - 20, player.x + step); // 80% of the viewport width
+          break;
+        case ' ':
+          if (!player.isJumping) {
+            player.isJumping = true;
+            player.y -= 50; // Adjust the jump height as needed
+            setTimeout(() => {
+              player.y += 50; // Reset the position after the jump
+              player.isJumping = false;
+            }, 1000); // Adjust the duration of the jump as needed (1 second in this case)
+          }
           break;
       }
-
-      let player = document.getElementById('player');
-      player.style.top = playerTop + 'px';
-      player.style.left = playerLeft + 'px';
-  
-      checkCollision();
     }
-
-    
-  
-    function checkCollision() {
-      const playerRow = Math.floor(playerTop / 20);
-      const playerCol = Math.floor(playerLeft / 20);
-      const newCell = mazeData[playerRow][playerCol];
-  
-      if (newCell === '#') {
-        // Undo the move if it collides with a wall
-        switch (direction) {
-          case 'up':
-            playerTop += 10;
-            break;
-          case 'down':
-            playerTop -= 10;
-            break;
-          case 'left':
-            playerLeft += 10;
-            break;
-          case 'right':
-            playerLeft -= 10;
-            break;
-        }
-      }
-  
-      if (newCell === 'E') {
-        alert('Congratulations! You reached the exit!');
-        resetGame();
-      }
-    }
-  
-    function resetGame() {
-      playerTop = 20;
-      playerLeft = 20;
-    }
-  
-    let mazeStyle = '';
-    onMount(() => {
-      mazeStyle = `
-        grid-template-columns: repeat(${mazeData[0].length}, 20px);
-        grid-template-rows: repeat(${mazeData.length}, 20px);
-      `;
-
-      window.addEventListener('keydown', (event) => {
-        const { key } = event;
-        console.log("🚀 ~ file: MazeGame.svelte:94 ~ window.addEventListener ~ event:", event)
-        console.log("🚀 ~ file: MazeGame.svelte:94 ~ window.addEventListener ~ key:", key)
-        
-      })
-    });
   </script>
   
   <style>
-    #maze {
-      display: grid;
-      gap: 1px;
-      border: 1px solid black;
-      background-color: white;
-      position: relative;
-      width: 400px;
-      height: 400px;
-      grid-template-columns: repeat(21, 20px);
-      grid-template-rows: repeat(20, 20px);
+    body {
+      margin: 0;
+      overflow: hidden;
     }
   
-    .cell {
-      width: 20px;
-      height: 20px;
-    }
-  
-    .wall {
-      background-color: black;
-    }
-  
-    #player {
-      background-color: green;
-      width: 20px;
-      height: 20px;
+    .canvas {
+      width: 80vw; /* 80% of the viewport width */
+      height: 80vh; /* 80% of the viewport height */
+      border: 1px solid #000;
+      background: linear-gradient(to bottom right, red, orange);
       position: absolute;
-      top: 10px;
-      left: 10px;
+      top: 10vh; /* 10% of the viewport height from the top */
+      left: 10vw; /* 10% of the viewport width from the left */
+    }
+  
+    .player {
+      width: 20px;
+      height: 20px;
+      background-color: green;
+      border-radius: 50%; /* Make it a perfect circle */
+      position: absolute;
+      top: 50px;
+      left: 50px;
+      transition: top 0.2s, left 0.2s; /* Smooth movement transition */
     }
   </style>
   
-  <div id="maze" style={mazeStyle}>
-    {#each mazeData as row, rowIndex}
-      {#each row as cell, colIndex}
-        <div
-          class="cell"
-          class:wall={cell === '#'}
-        ></div>
-      {/each}
-    {/each}
-  
-    <div id="player" on:keydown={(event) => movePlayer(event.key)}></div>
+  <div class="canvas" tabindex="0" on:keydown={handleKeyPress}>
+    <div class="player" style="top: {player.y}px; left: {player.x}px;"></div>
   </div>
   
--- src/routes/exercises/2023-12-17/+page.svelte
+++ src/routes/exercises/2023-12-17/+page.svelte
@@ -1,101 +1,114 @@
 <script>
 	import { onMount } from 'svelte';
-  
+
 	let canvas;
 	let context;
 	let squareX = 0;
 	let squareY = 0;
 	let targetX = 0;
 	let targetY = 0;
-  
+
 	const movementSpeed = 4;
 	let isTouchingBorder = false;
-  
+
 	function draw() {
-	  context.clearRect(0, 0, canvas.width, canvas.height);
-	  const squareSize = 25;
-  
-	  context.beginPath();
-	  context.rect(squareX, squareY, squareSize, squareSize);
-  
-	  // Set the color based on whether the square is touching the border
-	  const color = isTouchingBorder ? 'red' : 'blue';
-  
-	  context.fillStyle = color;
-	  context.fill();
+		context.clearRect(0, 0, canvas.width, canvas.height);
+		const squareSize = 25;
+
+		context.beginPath();
+		context.rect(squareX, squareY, squareSize, squareSize);
+
+		// Set the color based on whether the square is touching the border
+		const color = isTouchingBorder ? 'red' : 'blue';
+
+		context.fillStyle = color;
+		context.fill();
 	}
-  
-	function update() {
-	  squareX += (targetX - squareX) / movementSpeed;
-	  squareY += (targetY - squareY) / movementSpeed;
-  
-	  draw();
-  
-	  if (Math.abs(squareX - targetX) > 0.1 || Math.abs(squareY - targetY) > 0.1) {
-		requestAnimationFrame(update);
-	  }
-	}
-  
+
 	onMount(() => {
-	  canvas = document.getElementById('test-canvas');
-	  context = canvas.getContext('2d');
-	  draw();
-	  window.addEventListener('keydown', handleKeyDown);
-  
-	  return () => {
-		window.removeEventListener('keydown', handleKeyDown);
-	  };
+		if (mazeData && mazeData.length > 0 && mazeData[0]) {
+			mazeStyle = `
+      grid-template-columns: repeat(${mazeData[0].length}, 20px);
+      grid-template-rows: repeat(${mazeData.length}, 20px);
+    `;
+		}
+
+		window.addEventListener('keydown', (event) => {
+			const { key } = event;
+			movePlayer(key);
+		});
 	});
-  
+
+	function update() {
+		squareX += (targetX - squareX) / movementSpeed;
+		squareY += (targetY - squareY) / movementSpeed;
+
+		draw();
+
+		if (Math.abs(squareX - targetX) > 0.1 || Math.abs(squareY - targetY) > 0.1) {
+			requestAnimationFrame(update);
+		}
+	}
+
+	onMount(() => {
+		canvas = document.getElementById('test-canvas');
+		context = canvas.getContext('2d');
+		draw();
+		window.addEventListener('keydown', handleKeyDown);
+
+		return () => {
+			window.removeEventListener('keydown', handleKeyDown);
+		};
+	});
+
 	function handleKeyDown(event) {
-	  if (event.key === 'ArrowRight') {
-		targetX = Math.min(canvas.width - 25, squareX + 20);
-	  } else if (event.key === 'ArrowLeft') {
-		targetX = Math.max(0, squareX - 20);
-	  } else if (event.key === 'ArrowDown') {
-		targetY = Math.min(canvas.height - 25, squareY + 20);
-	  } else if (event.key === 'ArrowUp') {
-		targetY = Math.max(0, squareY - 20);
-	  }
-  
-	  // Check if the square is touching the border after the update
-	  isTouchingBorder = checkTouchingBorder();
-	  
-	  update(); // Start the smooth transition
+		if (event.key === 'ArrowRight') {
+			targetX = Math.min(canvas.width - 25, squareX + 20);
+		} else if (event.key === 'ArrowLeft') {
+			targetX = Math.max(0, squareX - 20);
+		} else if (event.key === 'ArrowDown') {
+			targetY = Math.min(canvas.height - 25, squareY + 20);
+		} else if (event.key === 'ArrowUp') {
+			targetY = Math.max(0, squareY - 20);
+		}
+
+		// Check if the square is touching the border after the update
+		isTouchingBorder = checkTouchingBorder();
+
+		update(); // Start the smooth transition
 	}
-  
+
 	function checkTouchingBorder() {
-	  const squareSize = 25;
-	  return (
-		squareX <= 0 ||
-		squareX + squareSize >= canvas.width ||
-		squareY <= 0 ||
-		squareY + squareSize >= canvas.height
-	  );
+		const squareSize = 25;
+		return (
+			squareX <= 0 ||
+			squareX + squareSize >= canvas.width ||
+			squareY <= 0 ||
+			squareY + squareSize >= canvas.height
+		);
 	}
-  </script>
-  
-  <div id="test-canvas-container">
+</script>
+
+<div id="test-canvas-container">
 	<canvas id="test-canvas"></canvas>
-  </div>
-  
-  <style>
+</div>
+
+<style>
 	#test-canvas-container {
-	  display: flex;
-	  align-items: center;
-	  justify-content: center;
-	  height: 100vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 100vh;
 	}
-  
+
 	#test-canvas {
-	  width: 90rem;
-	  height: 50rem;
-	  border: 6px solid wheat;
-	  position: absolute;
-	  top: 50%;
-	  left: 50%;
-	  transform: translate(-50%, -50%);
-	  background: white;
+		width: 90rem;
+		height: 50rem;
+		border: 6px solid wheat;
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		background: white;
 	}
-  </style>
-  
+</style>
